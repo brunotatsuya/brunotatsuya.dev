267e81d8ab41441f5f1f43b9780f032f2cbc0cb3
Bruno Tatsuya <brunomasunaga@gmail.com>
2026-02-08 13:57:49 +0100

    Prevent indexing logos and resumes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/logos/:path*',
+        headers: [
+          { key: 'X-Robots-Tag', value: 'noindex, nofollow, nosnippet' },
+        ],
+      },
+      {
+        source: '/resumes/:path*',
+        headers: [
+          { key: 'X-Robots-Tag', value: 'noindex, nofollow, nosnippet' },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
